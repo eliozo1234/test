025d135e2dec5944b5e1395c1d277ba1276260d4
eliozo1234 <eliozobateli@gmail.com>
2025-01-17 22:01:28 +0100

    Add files via upload

--- ai-imagegenerator.py
+++ ai-imagegenerator.py
@@ -14,7 +14,7 @@ model_id = "stabilityai/stable-diffusion-2-1"
 
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
 pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
-pipe = pipe.to("cuda")
+pipe = pipe.to("cpu")
 
 @app.route("/", methods=["GET", "POST"])
 def index():
